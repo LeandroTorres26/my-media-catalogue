83bf5caaa7dbfe7121feefb1d6b2b125eaa09ac6
Leandro Torres <leandro.torres@agencian1.com.br>
2025-03-25 01:10:06 -0300

    style: DaisyUi implementation

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -29,34 +29,81 @@ export default function Login() {
     <div className="grid h-screen w-full place-items-center px-4">
       <form
         onSubmit={loginUser}
-        className="grid w-full max-w-[25rem] gap-6 self-center rounded-2xl bg-neutral-900 px-6 py-4"
+        className="bg-base-100 grid w-full max-w-[25rem] gap-6 self-center rounded-2xl px-6 py-4"
       >
         {error && <div className="text-black">{error}</div>}
         <h1 className="justify-self-center text-2xl">Sign In</h1>
         <div className="flex flex-col gap-2">
           <label htmlFor="email">Email</label>
-          <input
-            type="email"
-            name="email"
-            placeholder="youremail@mail.com"
-            className="rounded-md px-2 py-1 text-sm text-black"
-          />
+          <div className="input validator">
+            <svg
+              className="h-[1em] opacity-50"
+              xmlns="http://www.w3.org/2000/svg"
+              viewBox="0 0 24 24"
+            >
+              <g
+                strokeLinejoin="round"
+                strokeLinecap="round"
+                strokeWidth="2.5"
+                fill="none"
+                stroke="currentColor"
+              >
+                <rect width="20" height="16" x="2" y="4" rx="2"></rect>
+                <path d="m22 7-8.97 5.7a1.94 1.94 0 0 1-2.06 0L2 7"></path>
+              </g>
+            </svg>
+            <input
+              type="email"
+              name="email"
+              placeholder="mail@site.com"
+              required
+            />
+          </div>
+          <div className="validator-hint hidden">Enter valid email address</div>
         </div>
         <div className="flex flex-col gap-2">
           <label htmlFor="password">Password</label>
-          <input
-            type="password"
-            name="password"
-            placeholder="password"
-            className="rounded-md px-2 py-1 text-sm text-black"
-          />
+          <div className="input validator">
+            <svg
+              className="h-[1em] opacity-50"
+              xmlns="http://www.w3.org/2000/svg"
+              viewBox="0 0 24 24"
+            >
+              <g
+                strokeLinejoin="round"
+                strokeLinecap="round"
+                strokeWidth="2.5"
+                fill="none"
+                stroke="currentColor"
+              >
+                <path d="M2.586 17.414A2 2 0 0 0 2 18.828V21a1 1 0 0 0 1 1h3a1 1 0 0 0 1-1v-1a1 1 0 0 1 1-1h1a1 1 0 0 0 1-1v-1a1 1 0 0 1 1-1h.172a2 2 0 0 0 1.414-.586l.814-.814a6.5 6.5 0 1 0-4-4z"></path>
+                <circle cx="16.5" cy="7.5" r=".5" fill="currentColor"></circle>
+              </g>
+            </svg>
+            <input
+              type="password"
+              name="password"
+              required
+              placeholder="Password"
+              minLength={8}
+              pattern="(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{8,}"
+              title="Must be more than 8 characters, including number, lowercase letter, uppercase letter"
+            />
+          </div>
+          <p className="validator-hint hidden">
+            Must be more than 8 characters, including
+            <br />
+            At least one number
+            <br />
+            At least one lowercase letter
+            <br />
+            At least one uppercase letter
+          </p>
         </div>
 
-        <div className="flex flex-col items-center gap-2">
-          <button className="mx-auto w-fit rounded-md border border-white px-4 py-2 hover:border-main-500 hover:bg-main-500 hover:text-black">
-            Sign In
-          </button>
-          <Link href="/register" className="hover:underline">
+        <div className="flex flex-col items-center">
+          <button className="btn btn-primary">Sign Up</button>
+          <Link href="/register" className="btn btn-link btn-neutral">
             Do not have an account?
           </Link>
         </div>
